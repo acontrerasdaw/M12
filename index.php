4992f30d30c8d8ef5b269992575cc3c94aa195db
alumne <alumne@alumne.alumne>
2017-06-05 19:33:18 +0200

    dia 0 Final

--- index.php
+++ index.php
@@ -2,14 +2,56 @@
 <!--
 
 -->
-<html>
+<html lang="es">
     <head>
+        <link rel="stylesheet" type="text/css" href="./css/stil.css">
         <meta charset="UTF-8">
-        <title></title>
+        <title>Training Book-Self Mugendo</title>
     </head>
     <body>
-        <?php
-echo "hola";
-        ?>
+        <div class="Cabecera">
+            <div class="titulo">
+                <h1>Training Book-Self</h1>
+            </div>
+            <div class="logo" >
+                <IMG SRC="./img/logo.png" alt="Logo Mugendo"/>
+            </div>
+        </div>
+
+        <div class="Menu">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#reservas">Reserva</a></li>
+                <li><a href="#novedades">Novedades</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </div>
+
+        <div class="Contenedor">
+            <div class="post1 bloque">
+                <IMG SRC="./img/judo.jpg" class="imagenoticia" alt="Imagen Judo"/>
+                <a href="#" class="noticia"><p>Judo para conectar con el mundo</p></a>
+            </div>
+
+
+            <div class="post2 bloque" >
+                <IMG SRC="./img/defensa.jpg"  class="imagenoticia" alt="Imagen defensa"/>
+                <a href="#" class="noticia"><p>Mujeres que usan los puños contra la violencia machista</p></a> 
+            </div>
+
+
+
+            <div class="post3 bloque" >
+                <IMG SRC="./img/tae.jpg" class="imagenoticia" alt="Imagen Taekwondo"/>
+                <a href="#" class="noticia"><p>La cosecha del taekwondo</p></a>  
+            </div>
+
+
+            <div class="post4 bloque" >
+                <IMG SRC="./img/aiki.jpg" class="imagenoticia" alt="Imagen aikido"/>
+                <a href="#" class="noticia"><p>Nuevas clases de aikidō</p></a>  
+            </div>
+        </div>
+
     </body>
 </html>
