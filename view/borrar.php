f9582d73874118c293522bf656819aeb8a81ecd3
daw <daw@PCDAW>
2017-06-08 17:40:45 +0200

    Dia 2 borrar.php

--- view/borrar.php
+++ view/borrar.php
@@ -1,3 +1,5 @@
 <?php
-
-
+$NumReserva=  filter_input(INPUT_GET, 'NumReserva');
+$query="DELETE FROM Reservas where NumReserva='".$NumReserva."'";
+echo $NumReserva;
+//header('Location: ./listado.php');
