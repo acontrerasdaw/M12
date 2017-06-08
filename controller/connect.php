68afb7c66bb75e8892bbe3948db5e4889197fe54
jaumealonso21 <jaumealonso@hotmail.com>
2017-06-08 16:30:59 +0200

    versió 4.0 Qüasi Final!!!

--- controller/connect.php
+++ controller/connect.php
@@ -8,19 +8,22 @@ if ($mysqli->connect_error) {
 //header
 $query = "INSERT INTO Reservas (NomReserva, emailReserva, telefonoReserva, arteReserva, fechaHoraReserva) VALUES ('" . $nomReserva . "','" . $emailReserva_Sano . "'," . $telefonoReserva . ",'" . $arteReserva . "','" . $fechaHoraReserva . "')";
 //$tildes=$mysqli->query("SET NAMES 'utf8'");
-$query02 = "";
+//$query02 = "";
 
 try {
     $url = filter_input(INPUT_SERVER, "REQUEST_URI");
     // executar la consulta depenent la pàgina on es situï
     switch ($url) {
-        case "/M12/view/comprovar.php":
+        case "/M12/view/reserva.php":
             $mysqli->query($query);
             header('Location: ../view/correcto.php');
             break;
         case "/M12/view/login2.php":
             //$mysqli->query($query02);
             echo 'muuu bien!!!';
+            break;
+        default:
+            
             break;
     }
 } catch (Exception $e) {
